a3b7c7e78ca1db3f1053cdf9f8dca690e9e79b3d
smeronek264 <sophie.meronek@drake.edu>
2025-04-06 13:06:29 -0500

    created file system

---
+++ flaskapp.py
@@ -0,0 +1,15 @@
+# author: T. Urness and M. Moore
+# description: Flask example using redirect, url_for, and flash
+# credit: the template html files were constructed with the help of ChatGPT
+
+from flask import Flask
+from flask import render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key' # this is an artifact for using flash displays; 
+                                   # it is required, but you can leave this alone
+
+@app.route('/')
+def home():
+    return render_template('home.html')
